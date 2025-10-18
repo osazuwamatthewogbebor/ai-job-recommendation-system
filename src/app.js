b192a356aa49634b6d50dca674da79ebadff0a32
Sultan Abdussalam <abdussultan14@gmail.com>
2025-10-18 20:42:11 +0100

    Implemented AI formatting and Job fetching

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import APP_CONFIG from './config/APP_CONFIG.js';
 import AppError from './utils/AppError.js';
-import logger from './config/logger.js';
+//import logger from './config/logger.js';
+import jobRoute from './routes/jobRoutes.js';
+import { recommendJobs } from './controllers/jobControllers.js';
 
 const port = APP_CONFIG.PORT;
 // All env and configuration files can be gotten from APP_CONFIG
@@ -13,12 +15,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', jobRoute);
+
+app.get('/', (req, res) => {
+  res.send('AI Job Recommendation API is running...');
+});
+
+
+
 app.listen(port, () => {
     // We will use pino logger here, anywhere we are supposed to use console.log
     // console log for now till it is configured on it
     // console.log(`Server is running on port ${port}`);
     // Pino in use after configuring
-    logger.info(`Server is running on port ${port}`)
+
+    //logger.info(`Server is running on port ${port}`)
 
 
     // this how to use the custom Error class instead of the default error class
---
+++ src/services/jobRecommendationService.js
@@ -0,0 +1,22 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
+import axios from 'axios';
+
+async function fetchJobs(query, country, page = 1) {
+  try {
+    const response = await axios.get('https://jsearch.p.rapidapi.com/search', {
+      params: { query, country, page },
+      headers: {
+        'X-RapidAPI-Key': process.env.JSEARCH_API_KEY,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      }
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error('Error fetching jobs:', error.message);
+    return [];
+  }
+}
+
+export {fetchJobs}; 

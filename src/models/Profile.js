23307c88f123adf6781fb0f33dd2067999f81df4
Folarex10 <timothy.olaleye18@gmail.com>
2025-10-20 13:51:50 +0100

    auth/Profile Controller, auth/Profile Service, auth/profile Route and Profile.js updated

---
+++ src/models/Profile.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelize.js";
+import User from "./User.js";
+
+const Profile = sequelize.define("Profile", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+  },
+  skills: {
+    type: DataTypes.STRING,
+  },
+  experience: {
+    type: DataTypes.STRING,
+  },
+  resume: {
+    type: DataTypes.STRING,
+  },
+});
+
+Profile.belongsTo(User, { foreignKey: "userId", onDelete: "CASCADE" });
+
+export default Profile;

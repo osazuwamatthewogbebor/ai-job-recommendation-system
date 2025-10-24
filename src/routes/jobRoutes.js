4447b45ea89a4d86307a6d804a0727260914e25d
Sultan Abdussalam <abdussultan14@gmail.com>
2025-10-24 22:50:24 +0100

    AI analyzer

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,18 +1,29 @@
 import express from 'express';
-// import jobController from '../controllers/jobController.js';  
 const router = express.Router();
 import { recommendJobs } from '../controllers/jobControllers.js';
 import { fetchJobs } from '../services/jobRecommendationService.js';
 import logger from '../config/logger.js';
+import Profile from '../models/Profile.js';
 
 
-// Example endpoint: POST /api/recommend
 router.post('/', async (req, res) => {
-  try {
-    const userProfile = req.body;
-    const jobs = await recommendJobs(userProfile);
-    res.json({ success: true, message: 'Job recommendations fetched successfully'});
+   try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({ success: false, message: 'userId is required' });
+    }
+    const userProfile = await Profile.findOne({ where: { id } });
+
+    if (!userProfile) {
+      return res.status(404).json({ success: false, message: 'Profile not found' });
+    }
+    const profileData = userProfile.get({ plain: true });
+    const jobs = await recommendJobs(profileData);
+
+    res.status(200).json(jobs);
+
   } catch (err) {
+    console.error('Error in /api/recommend:', err);
     res.status(500).json({ success: false, error: err.message });
   }
 });

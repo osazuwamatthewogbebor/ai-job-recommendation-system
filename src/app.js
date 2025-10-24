7740fce88981a2145c35e860766ec5f521ac958b
osazuwamatthewogbebor <osazuwamatthewogbebor@gmail.com>
2025-10-25 00:13:31 +0100

    cleared excess comments from app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,11 @@
 import express from 'express';
 import APP_CONFIG from './config/APP_CONFIG.js';
 import logger from './config/logger.js';
-import emailService from './services/emailService.js';
 import sequelize from './config/sequelize.js';
-import job from './models/job.js';
-import resume from './models/Resume.js';
 import uploadRoutes from './routes/uploadRoutes.js'; 
-import user from './models/User.js';
 import apiLimiter from "./middleware/rateLimiter.js";
 import jobRoute from './routes/jobRoutes.js';
-import { recommendJobs } from './controllers/jobControllers.js';
-import Profile from './models/Profile.js';
 
-
-// All env and configuration files can be gotten from APP_CONFIG
-// when you add sth to .env, also put in APP_CONFIG
-// This ensures we have all credentials in one source of truth
-
-// dotenv.config();
 
 const app = express();
 
@@ -49,16 +37,5 @@ sequelize.sync()
 app.use('/api/recommend', jobRoute);
 
 app.listen(port, () => { 
-    // We will use pino logger here, anywhere we are supposed to use console.log
-    // console log for now till it is configured on it
-    // console.log(`Server is running on port ${port}`);
-    // Pino in use after configuring
-
     logger.info(`Server is running on port ${port}`)
-
-
-    // this how to use the custom Error class instead of the default error class
-    // throw new AppError(`Testing the custom error class`, 401)
-    // whenever you need throw new Error(), use the AppError instead
-    // It takes the message and statusCode making it more precise than the default new Error
 ;});

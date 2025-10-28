72db0e2c17cf77878fcaea6661d1f7184d023385
Sultan Abdussalam <abdussultan14@gmail.com>
2025-10-28 15:31:43 +0100

    API fetch past month

--- src/utils/jobFetcher.js
+++ src/utils/jobFetcher.js
@@ -6,7 +6,7 @@ import logger from '../config/logger.js';
 async function fetchJobs(query, country, work_from_home, page = 1) {
   try {
     const response = await axios.get('https://jsearch.p.rapidapi.com/search', {
-      params: { query, country, work_from_home,page },
+      params: { query, country, work_from_home, date_posted: 'month', page },
       headers: {
         'X-RapidAPI-Key': APP_CONFIG.JSEARCH_API_KEY,
         'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
